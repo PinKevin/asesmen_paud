13c902fab74f5c04cf0a475cb833f06837e81ffe
Emerio Kevin Aryaputra <emerio.kevin@gmail.com>
2025-05-14 09:43:16 +0700

    fix: update logout snackbar and improve MenuButton widget structure

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:asesmen_paud/api/service/auth_service.dart';
+import 'package:asesmen_paud/widget/color_snackbar.dart';
 import 'package:asesmen_paud/widget/dashboard/greeting.dart';
 import 'package:asesmen_paud/widget/dashboard/menu_button.dart';
 import 'package:flutter/material.dart';
@@ -41,12 +42,14 @@ class DashboardPageState extends State<DashboardPage> {
 
       if (!mounted) return;
       ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Anda berhasil sign out')));
+        ColorSnackbar.build(message: 'Anda berhasil sign-out', success: true),
+      );
 
       Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
     } catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Failed to logout: $e')));
+      ScaffoldMessenger.of(context).showSnackBar(
+        ColorSnackbar.build(message: '$e', success: false),
+      );
     } finally {
       setState(() {
         _isLoading = false;
@@ -61,92 +64,117 @@ class DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<String?>(
-        future: _teacherName,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-          final teacherName = snapshot.data ?? 'Guru';
+      future: _teacherName,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        final teacherName = snapshot.data ?? 'Guru';
 
-          return Scaffold(
-              appBar: AppBar(
-                title: const Text('Dashboard'),
-                actions: [
-                  IconButton(
-                      onPressed: _logout,
-                      icon: _isLoading
-                          ? const SizedBox(
-                              width: 20,
-                              height: 20,
-                              child: Center(
-                                child: CircularProgressIndicator(),
-                              ),
-                            )
-                          : const Icon(Icons.logout))
-                ],
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Greeting(teacherName: teacherName),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        MenuButton(
-                            icon: Icons.create,
-                            label: 'Anekdot',
-                            onPressed: () => _navigateToStudents(
-                                arguments: {'mode': 'anecdotal'})),
-                        MenuButton(
-                            icon: Icons.palette,
-                            label: 'Hasil Karya',
-                            onPressed: () => _navigateToStudents(
-                                arguments: {'mode': 'artwork'}))
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          MenuButton(
-                              icon: Icons.check,
-                              label: 'Ceklis',
-                              onPressed: () => _navigateToStudents(
-                                  arguments: {'mode': 'checklist'})),
-                          MenuButton(
-                              icon: Icons.camera_alt,
-                              label: 'Foto Berseri',
-                              onPressed: () => _navigateToStudents(
-                                  arguments: {'mode': 'series-photo'}))
-                        ]),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          MenuButton(
-                              icon: Icons.article,
-                              label: 'Laporan Bulanan',
-                              onPressed: () => _navigateToStudents(
-                                  arguments: {'mode': 'report'}))
-                        ]),
-                  ],
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Dashboard'),
+            actions: [
+              IconButton(
+                onPressed: _logout,
+                icon: _isLoading
+                    ? const SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      )
+                    : const Icon(Icons.logout),
+              )
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Greeting
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Greeting(teacherName: teacherName),
                 ),
-              ));
-        });
+                const SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 20,
+                    childAspectRatio: 1,
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 10,
+                    ),
+                    children: [
+                      MenuButton(
+                        icon: Icons.create,
+                        label: 'Anekdot',
+                        onPressed: () => _navigateToStudents(
+                          arguments: {'mode': 'anecdotal'},
+                        ),
+                      ),
+                      MenuButton(
+                        icon: Icons.palette,
+                        label: 'Hasil Karya',
+                        onPressed: () => _navigateToStudents(
+                          arguments: {'mode': 'artwork'},
+                        ),
+                      ),
+                      MenuButton(
+                        icon: Icons.check,
+                        label: 'Ceklis',
+                        onPressed: () => _navigateToStudents(
+                          arguments: {'mode': 'checklist'},
+                        ),
+                      ),
+                      MenuButton(
+                        icon: Icons.camera_alt,
+                        label: 'Foto Berseri',
+                        onPressed: () => _navigateToStudents(
+                          arguments: {'mode': 'series-photo'},
+                        ),
+                      ),
+                      MenuButton(
+                        icon: Icons.article,
+                        label: 'Laporan Bulanan',
+                        onPressed: () => _navigateToStudents(
+                          arguments: {'mode': 'report'},
+                        ),
+                      ),
+                      MenuButton(
+                        icon: Icons.person,
+                        label: 'Murid',
+                        onPressed: () => _navigateToStudents(
+                          arguments: {'mode': 'student'},
+                        ),
+                      ),
+                      MenuButton(
+                        icon: Icons.info,
+                        label: 'Murid',
+                        onPressed: () => _navigateToStudents(
+                          arguments: {'mode': 'student'},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
--- lib/widget/dashboard/menu_button.dart
+++ lib/widget/dashboard/menu_button.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MenuButton extends StatelessWidget {
   final IconData icon;
   final String label;
-  final dynamic Function() onPressed;
+  final VoidCallback onPressed;
 
   const MenuButton(
       {super.key,
@@ -13,29 +13,42 @@ class MenuButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: onPressed,
-          style: ElevatedButton.styleFrom(
-            shape: const CircleBorder(),
-            padding: const EdgeInsets.all(20),
-            backgroundColor: Colors.deepPurple,
-            foregroundColor: Colors.white,
+    return SizedBox(
+      width: 100,
+      height: 150,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: 70,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: onPressed,
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: EdgeInsets.zero,
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white,
+              ),
+              child: Icon(
+                icon,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
           ),
-          child: Icon(
-            icon,
-            color: Colors.white,
+          const SizedBox(
+            height: 8,
           ),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        Text(
-          label,
-          style: const TextStyle(fontSize: 16),
-        )
-      ],
+          Text(
+            label,
+            style: const TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            overflow: TextOverflow.fade,
+          )
+        ],
+      ),
     );
   }
 }
